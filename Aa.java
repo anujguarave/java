ce1b586bead799d86ff900ef5109efbb78da21ae
anuj <anuj@gmail.com>
2023-06-14 17:40:52 +0530

    first

---
+++ Aa.java
@@ -0,0 +1,11 @@
+public class Aa {
+    public static void main(String[] args) {
+        System.out.println("anuj gaurave !");
+        int a = 4;
+        int b = 7;
+        int c = 88;
+        int sum = a + b + c;
+        System.out.println(sum);
+
+    }
+}
